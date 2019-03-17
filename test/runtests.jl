31c762920fe9895968c62b61b66cbcfdb6d2fb5c
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2019-03-17 15:05:14 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,15 +6,15 @@ GtkREPL.gtkrepl()
 
 @testset "Console" begin 
     c = GtkREPL.main_window.console_manager[1]
-    @assert c.prompt_position == length(c.prompt)+1
-    @assert GtkREPL.command(c) == ""
+    @test c.prompt_position == length(c.prompt)+1
+    @test GtkREPL.command(c) == ""
 
     GtkREPL.command(c,"x=2")
-    @assert GtkREPL.command(c) == "x=2"
-    @assert c.prompt_position == length(c.prompt)+1
+    @test GtkREPL.command(c) == "x=2"
+    @test c.prompt_position == length(c.prompt)+1
 
     GtkREPL.on_return(c,GtkREPL.command(c))
     sleep(0.1)
-    @assert GtkREPL.command(c) == ""
-    @assert c.prompt_position == 20
+    @test GtkREPL.command(c) == ""
+    @test c.prompt_position == 20
 end
