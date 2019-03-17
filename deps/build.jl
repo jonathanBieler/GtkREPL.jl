4c8d8a7725c7760011031bdffcf6cf37fb8b271d
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2019-03-17 14:36:15 +0100

    trying to fix build

---
+++ deps/build.jl
@@ -0,0 +1,6 @@
+# add unregistered pacakages
+using Pkg
+Pkg.add("https://github.com/jonathanBieler/GtkExtensions.jl.git")
+Pkg.add("https://github.com/jonathanBieler/RemoteGtkREPL.jl.git")
+Pkg.add("https://github.com/jonathanBieler/JuliaWordsUtils.jl.git")
+Pkg.add("https://github.com/jonathanBieler/GtkTextUtils.jl.git")
