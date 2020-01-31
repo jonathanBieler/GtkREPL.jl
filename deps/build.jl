78832933f02d7aa5447a21b3ce12166a99e6eb07
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2020-01-31 14:00:20 +0100

    removed deps

--- deps/build.jl
+++
@@ -1,6 +0,0 @@
-# add unregistered pacakages
-using Pkg
-Pkg.add(PackageSpec(url="https://github.com/jonathanBieler/GtkExtensions.jl.git", rev="master"))
-Pkg.add(PackageSpec(url="https://github.com/jonathanBieler/RemoteGtkREPL.jl.git", rev="master"))
-Pkg.add(PackageSpec(url="https://github.com/jonathanBieler/JuliaWordsUtils.jl.git", rev="master"))
-Pkg.add(PackageSpec(url="https://github.com/jonathanBieler/GtkTextUtils.jl.git", rev="master"))
